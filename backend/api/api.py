f37bdcf05765b6f95b466727bf6bfb6a910bcb0c
ksarmentrout <k.armentrout11@gmail.com>
2020-03-19 23:20:07 -0400

    Added random entry endpoint

--- backend/api/api.py
+++ backend/api/api.py
@@ -3,6 +3,7 @@ from flask_restful import (
     Api,
     Resource,
 )
+from sqlalchemy.sql.expression import func
 
 from .models import (
     to_dict,
@@ -18,4 +19,29 @@ class Tweet(Resource):
         return jsonify([to_dict(tweet) for tweet in TweetModel.query.all()])
 
 
+class RandomTweets(Resource):
+    def get(self):
+        '''
+        The performance of this method will degrade once db becomes large because it
+        requires a full db scan. May need to change how it's done.
+        Another method would be to add a random number column to the db and sort by
+        some offset of that column.
+        '''
+        tweets = TweetModel.query.order_by(func.random()).limit(200).all()
+        return jsonify([to_dict(tweet) for tweet in tweets])
+
+
+# class OptimizedRandomTweets(Resource):
+#     def get(self):
+#         tweets = TweetModel.query.options(load_only('id')).offset(
+#             func.floor(
+#                 func.random() *
+#                 db.session.query(func.count(TweetModel.id))
+#             )
+#         ).limit(1).all()
+#         return jsonify([to_dict(tweet) for tweet in tweets])
+
+
 api.add_resource(Tweet, '/')
+api.add_resource(RandomTweets, '/random')
+# api.add_resource(OptimizedRandomTweets, '/optimized_random')
