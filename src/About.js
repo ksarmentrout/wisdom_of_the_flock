190e8d4206e66ec211f0ee622ee384753e879a4a
ksarmentrout <k.armentrout11@gmail.com>
2020-04-29 22:41:32 -0400

    Added about page content

--- src/About.js
+++ src/About.js
@@ -17,16 +17,41 @@ class About extends App {
                     <div className="about_content">
                         <h1 id="about_title">About</h1>
                         <p>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc posuere ullamcorper ipsum, vel faucibus libero posuere sed. Fusce quis augue egestas, sodales sapien id, accumsan lectus. Donec et vehicula odio, at sodales ligula. Ut sit amet ultrices ante. Ut a pharetra dolor. Pellentesque maximus iaculis dapibus. Sed rutrum, urna quis rutrum dignissim, quam erat volutpat risus, nec volutpat velit lectus in metus. Phasellus eleifend convallis nunc, sit amet fermentum metus tristique ac. Duis condimentum suscipit ligula, ac faucibus nibh iaculis eu. Aenean sed pellentesque risus, at volutpat lorem. Praesent ultrices enim dui, sit amet lobortis arcu sollicitudin non.
+                            Wisdom of the Flock investigates how people use their Twitter audience as a collective
+                            intelligence to answer primarily moral questions.
                         </p>
                         <p>
-    Vivamus feugiat velit vel turpis eleifend, id hendrerit diam dictum. Sed sed lorem porttitor, finibus metus et, dapibus nisl. Pellentesque congue velit in risus volutpat porttitor. Suspendisse vulputate magna sed velit ornare interdum. Pellentesque sem tellus, aliquam ac quam non, varius luctus risus. Phasellus efficitur gravida lobortis. Proin orci dolor, tempus id justo vel, convallis tristique quam. Cras porta quam finibus, aliquet urna in, tincidunt nisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam at varius dolor, sit amet porttitor nisl.
+                            Over February-April 2020, tweets posing "should" and "must" questions to the general public
+                            were collected, of which a subset was curated for inclusion here. Questions using this
+                            language ask how the world ought to be and what actions are morally permissible, reaching
+                            for some authority to provide the answer. By displaying some of them here, Wisdom of the
+                            Flock looks at the types of questions that people trust the public to respond to as that
+                            authority.
                         </p>
                         <p>
-    Vestibulum efficitur convallis porttitor. Nulla venenatis elementum urna ut tempor. Suspendisse a leo vitae sapien efficitur consequat sit amet vitae libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisi neque, suscipit at tellus id, fermentum mattis dolor. Nulla semper in ante id placerat. Fusce viverra molestie dui quis malesuada. Duis dapibus nunc sit amet consequat convallis. Nunc porta imperdiet placerat. Donec rhoncus orci nec nisi finibus, in efficitur sem pharetra.
+                            This project began in 2019 but was completed in 2020 in the midst of a global pandemic.
+                            This backdrop of uncertainty, anxiety, sorrow, and restlessness is reflected in the full
+                            corpus of collected tweets and provides crucial context for considering the types of
+                            questions being asked.
                         </p>
                         <p>
-    Nullam consequat vitae massa sed interdum. Mauris dapibus metus id orci malesuada imperdiet. Ut nibh ipsum, varius quis rhoncus ac, scelerisque vitae eros. Morbi euismod erat in scelerisque porttitor. Suspendisse urna nunc, porttitor eu blandit quis, sollicitudin et enim. Phasellus vitae arcu efficitur, commodo justo sit amet, iaculis augue. Aliquam interdum porttitor quam, sed vulputate nunc varius sed. Duis congue lacinia libero, sed ultrices magna lobortis a.
+                            The questions here range from lighthearted to rhetorical to deeply sincere, together
+                            displaying the range of answers we seek from other people.
+                        </p>
+                        <p>
+                            More information, including links to the full corpus of collected tweets, will be
+                            available soon.
+                        </p>
+                        <p className='ref_section'>
+                            Created by <a href="https://ksarmentrout.com" className='about_link'>
+                                Keaton Armentrout
+                            </a>.<br/>
+                            <a href="https://twitter.com/keaton_sa" className='about_link about_refs'>
+                                @keaton_sa
+                            </a><br/>
+                            <a href="mailto:info@wisdomoftheflock.com" className='about_link about_refs'>
+                                info@wisdomoftheflock.com
+                            </a><br/>
                         </p>
                     </div>
                 </div>
