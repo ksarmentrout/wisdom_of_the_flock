4d29d4ff9cb1222881ab72db7e62a07c80f40f4c
ksarmentrout <k.armentrout11@gmail.com>
2020-11-20 13:23:21 -0800

    updated about page

--- src/About.js
+++ src/About.js
@@ -17,27 +17,27 @@ class About extends App {
                     <div className="about_content">
                         <h1 id="about_title">About</h1>
                         <p>
-                        Wisdom of the Flock is a curated collection of tweets posted over February-May 2020 that each 
+                        Wisdom of the Flock is a curated collection of tweets posted over March-May 2020 that each 
                         ask what "should" or "must" be. The collection as a whole investigates how people use their 
-                        Twitter audience as a collective intelligence to answer primarily moral questions.
+                        Twitter audience as a collective intelligence to answer questions.
                         </p>
                         <p>
-                        Questions using this language ask how the world ought to be and what actions are morally permissible, 
+                        Questions using this language ask how the world ought to be and what actions are morally acceptable, 
                         reaching for some authority to provide the answer. By displaying some of them here, Wisdom of the Flock 
-                        looks at the types of questions that people trust the public to respond to as that authority.
+                        looks at the types of questions people trust the public to respond to as that authority.
                         </p>
                         <p>
-                        This project began in 2019 but was completed in 2020 in the midst of a global pandemic. 
-                        This backdrop of uncertainty, anxiety, sorrow, and restlessness is reflected in the full corpus of 
-                        tweets and provides important context for looking at the scope of what is asked.
+                        This project began in 2019 but was completed in 2020 in the midst of a confluence of public health 
+                        and social justice crises. This backdrop of uncertainty, anxiety, sorrow, anger, and restlessness is 
+                        reflected in the full corpus of tweets and provides important context for the scope of what is asked.
                         </p>
                         <p>
                         The questions here vary from lighthearted to rhetorical to deeply sincere, together displaying 
                         the range of answers and guidance we seek from other people. 
                         </p>
                         <p>
-                        More information, including links to both the full corpus of collected tweets and the curated 
-                        subset on display, can be found here (link). If you recognize a tweet here as your own and would 
+                        More information, including links to both the full set of collected tweets and the curated 
+                        subset on display, will be made available soon. If you recognize a tweet here as your own and would 
                         like it to be either credited or removed, please contact me.
                         </p>
                         <p className='ref_section'>
